d1d5fc016636301a2f2de1dc63b66d6d5db80b8e
Adam Stankiewicz <agstanki@gmail.com>
2021-08-23 14:19:46 -0400

    initial commit

---
+++ packages/hooks/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>My Tasks</h1>
+      </header>
+      <main>
+        <ul>
+          <li className="d-flex align-items-center">
+            <input type="checkbox" id="delectus aut autem" value={1} />
+            <label for="delectus aut autem">delectus aut autem</label>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
